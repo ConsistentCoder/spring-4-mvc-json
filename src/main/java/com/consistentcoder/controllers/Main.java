a66cceef93f7a0c52a027829f601e9723e8e8aa1
ConsistentCoder <consistentcoder@gmail.com>
2015-11-30 17:51:16 +0800

    Added the page/link where the JSON message response will be displayed.

--- src/main/java/com/consistentcoder/controllers/Main.java
+++ src/main/java/com/consistentcoder/controllers/Main.java
@@ -1,9 +1,15 @@
 package com.consistentcoder.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.consistentcoder.json.Response;
 
 @Controller
 public class Main {
@@ -13,4 +19,14 @@ public class Main {
 		model.addAttribute("message", "Spring 4 MVC JSON Hello World Example Built with Maven");
 		return "hello";
 	}
+	
+	@RequestMapping(value="/json", method=RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Response jsonPage() {
+
+            List<String> result = new ArrayList<String>();
+            result.add("Hello World!");
+
+        return new Response(result);
+    }
 }
